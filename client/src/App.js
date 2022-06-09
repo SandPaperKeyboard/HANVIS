f4ad03e2cc69df2fc9702e94c397a69b1f9ba9d7
andrew828 <andy828@naver.com>
2022-06-10 02:48:01 +0900

    ClientServerSeparation

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+
+import Main from "./container/Main";
+import College from './container/College';
+import ServiceCenter from './container/ServiceCenter';
+import Survey from './container/Survey';
+import Result from './container/Result';
+import NextPage1 from './component/next1';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Main />} ></Route>
+            <Route path="/college" element={<College />} />
+
+            <Route path="/LiberalArts" element={<College />} />
+
+            <Route path="/next1" element={<NextPage1 />} />
+            <Route path="/service_center" element={<ServiceCenter />} />
+            <Route path="/survey/:surveyCode" element={<Survey />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ client/src/container/Survey.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios/index';
+
+import Header from "../component/Header";
+import SurveyQuestion from '../component/SurveyQuestion';
+
+const Survey = () => {    
+    return (
+        <div>
+            <Header />
+            <SurveyQuestion />
+        </div>
+    );
+}
+
+export default Survey;
+
+/*  학준 코딩 백업
+const Survey = () => {
+    let { surveyCode } = useParams();
+    const [department, setDepartment] = useState({});
+
+    useEffect(() => {
+        const apiUrl = '/dummy/college_list.json';
+
+        axios.get(apiUrl)
+        .then(table => {
+            setDepartment({
+                department : table.data.collegeList.find( 
+                    d => ( d.id === parseInt(surveyCode) ) 
+                )
+            });
+        }).catch(
+            error => {
+                console.log(error);
+            }
+        );
+    }, []);
+    
+    return (
+        <div>
+            <Header />
+            <SurveyQuestion props={department}/>
+        </div>
+    );
+} 
+*/
