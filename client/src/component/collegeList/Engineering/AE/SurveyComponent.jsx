ea4f1ed9ba4aa13c0af1155752ac247cc8b963db
andrew828 <andy828@naver.com>
2022-06-12 16:09:32 +0900

    ServerDownload

--- client/src/component/collegeList/Engineering/AE/SurveyComponent.jsx
+++ client/src/component/collegeList/Engineering/AE/SurveyComponent.jsx
@@ -1,7 +1,9 @@
 import "survey-core/defaultV2.css";
 import { StylesManager, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
-import AEJson from './AEJson.json'
+import AEJson from "./AEJson.json";
+import ReactDOM from "react-dom";
+import axios from 'axios'
 
 StylesManager.applyTheme("defaultV2");
 
@@ -10,4 +12,39 @@ function AESurveyComponent() {
   return <Survey model={survey} />;
 }
 
+
+
+
+
+function sendDataToServer(survey) {
+  //send Ajax request to your web server
+  alert("The results are: " + JSON.stringify(survey.data));
+}
+
+ReactDOM.render(
+<Survey.Survey json={surveyJSON} onComplete={sendDataToServer} />,
+document.getElementById("surveyContainer")
+);
+
+
+
+
+
+
 export default AESurveyComponent;
+
+
+
+/*
+StylesManager.applyTheme("defaultV2");
+
+function sendDataToServer(survey) {
+    //send Ajax request to your web server
+    alert("The results are: " + JSON.stringify(survey.data));
+}
+
+ReactDOM.render(
+  <Survey.Survey json={surveyJSON} onComplete={sendDataToServer} />,
+  document.getElementById("surveyContainer")
+);
+*/
